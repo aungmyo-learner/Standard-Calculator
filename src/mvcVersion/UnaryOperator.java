4ddcbed0d623e356470dd665901178263c4e9161
Aung Myo <aungmyo01062003@gmail.com>
2026-07-08 23:57:37 +0630

    Learn, how can still use enum class in fields

--- src/mvcVersion/UnaryOperator.java
+++ src/mvcVersion/UnaryOperator.java
@@ -1,8 +1,43 @@
 package mvcVersion;
 
 public enum UnaryOperator {
-	SQRT,
-	INVERSE,
-	SQR,
-	NEGATE;
+	SQRT {
+		public String format(String text) {
+			return "\u221A(" + text + ")";
+		}
+		
+		public double calculate(double value) {
+			return Math.sqrt(value);
+		}
+	},
+	INVERSE{
+		public String format(String text) {
+			return "1/(" + text + ")";
+		}
+		
+		public double calculate(double value) {
+			return 1/value;
+		}
+	},
+	SQR{
+		public String format(String text) {
+			return "sqr(" + text + ")";
+		}
+		
+		public double calculate(double value) {
+			return Math.pow(value, 2);
+		}
+	},
+	NEGATE{
+		public String format(String text) {
+			return "negate(" + text + ")";
+		}
+
+		public double calculate(double value) {
+			return -value;
+		}
+	};
+	public abstract String format(String text);
+	
+	public abstract double calculate(double value);
 }
