9bdd797b4522cfd3c8c1395ce858eae1bf4c02be
Aung Myo <aungmyo01062003@gmail.com>
2026-07-11 00:02:34 +0630

    09-07-2026 → fix methods

--- src/mvcVersion/UnaryOperator.java
+++ src/mvcVersion/UnaryOperator.java
@@ -1,43 +1,69 @@
 package mvcVersion;
 
 public enum UnaryOperator {
+	NOUNARY{
+
+		@Override
+		public String format(String text, DivisionNumber num) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public UnaryResult calculate(double value) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	},
 	SQRT {
-		public String format(String text) {
+		public String format(String text, DivisionNumber num) {
 			return "\u221A(" + text + ")";
 		}
 		
-		public double calculate(double value) {
-			return Math.sqrt(value);
+		public UnaryResult calculate(double value) {
+			if (value < 0) {
+				return new UnaryResult(value, true);
+			}
+			double result = Math.sqrt(value);
+			return new UnaryResult(result, false);
 		}
 	},
 	INVERSE{
-		public String format(String text) {
+		public String format(String text, DivisionNumber num) {
 			return "1/(" + text + ")";
 		}
 		
-		public double calculate(double value) {
-			return 1/value;
+		public UnaryResult calculate(double value) {
+			if (value == 0) {
+				return new UnaryResult(value, true);
+			}
+			double result = 1.0 / value;
+			return new UnaryResult(result, false);
 		}
 	},
 	SQR{
-		public String format(String text) {
+		public String format(String text, DivisionNumber num) {
 			return "sqr(" + text + ")";
 		}
 		
-		public double calculate(double value) {
-			return Math.pow(value, 2);
+		public UnaryResult calculate(double value) {
+			return new UnaryResult(Math.pow(value, 2), false);
 		}
 	},
 	NEGATE{
-		public String format(String text) {
-			return "negate(" + text + ")";
+		public String format(String text, DivisionNumber num) {
+			if (num == DivisionNumber.SECOND) {
+				return "negate(" + text + ")";
+			}
+			return text;
 		}
 
-		public double calculate(double value) {
-			return -value;
+		public UnaryResult calculate(double value) {
+			return new UnaryResult(-value, false);
 		}
 	};
-	public abstract String format(String text);
+	public abstract String format(String text, DivisionNumber num);
 	
-	public abstract double calculate(double value);
+	public abstract UnaryResult calculate(double value);
 }
